5d5554d418500f7a52470ae6cb976a7b0fab9d5e
Nataliia Zabiiaka <zabijaka.nata93@gmail.com>
2022-11-02 22:06:26 +0200

    Added a some new details of the project

--- lib/ui/pages/profile_screen.dart
+++ lib/ui/pages/profile_screen.dart
@@ -65,6 +65,39 @@ class ProfileScreen extends StatelessWidget {
               ),
               child: Container(
                 color: Colors.white,
+                padding: const EdgeInsets.only(
+                    top: 40, left: 32, right: 16, bottom: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: const Text(
+                        'Date, Year',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                      subtitle: const Text(
+                        'Hello, David',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: ClipOval(
+                        child: Image.asset(
+                          'assets/user.jpeg',
+                        ),
+                      ),
+                    ),
+                    _RadialProgress(
+                      width: height * 0.2,
+                      height: height * 0.2,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -123,19 +156,42 @@ class ProfileScreen extends StatelessWidget {
                         ),
                       ),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('YOUR NEXT WORKOUT'),
-                          Text('UPPER BODY'),
+                          const SizedBox(width: 20),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 16, left: 16),
+                            child: Text(
+                              'YOUR NEXT WORKOUT',
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 4, left: 16),
+                            child: Text(
+                              'Upper Body',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
                           Expanded(
                             child: Row(
                               children: [
+                                const SizedBox(width: 20),
                                 Container(
                                   decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(
-                                Radius.circular(25),
-                            ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(25),
+                                    ),
                                     color: Color(0xFF5B4D9D),
-                            ),
+                                  ),
                                   padding: const EdgeInsets.all(10),
                                   child: Image.asset(
                                     'assets/chest.png',
@@ -154,7 +210,7 @@ class ProfileScreen extends StatelessWidget {
                                   ),
                                   padding: const EdgeInsets.all(10),
                                   child: Image.asset(
-                                      'assets/back.png',
+                                    'assets/back.png',
                                     width: 50,
                                     height: 50,
                                     color: Colors.white,
@@ -170,7 +226,7 @@ class ProfileScreen extends StatelessWidget {
                                   ),
                                   padding: const EdgeInsets.all(10),
                                   child: Image.asset(
-                                      'assets/biceps.png',
+                                    'assets/biceps.png',
                                     width: 50,
                                     height: 50,
                                     color: Colors.white,
@@ -191,6 +247,48 @@ class ProfileScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class _RadialProgress extends StatelessWidget {
+  final double height, width;
+
+  const _RadialProgress({Key? key, required this.height, required this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _RadialPainter(progress: 0.7),
+      child: Container(
+        height: height,
+        width: width,
+      ),
+    );
+  }
+}
+
+class _RadialPainter extends CustomPainter {
+  final double progress;
+
+  _RadialPainter({required this.progress});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..strokeWidth = 10
+      ..color = const Color(0xFF200087)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    canvas.drawArc(Rect.fromCircle(center: center, radius: size.width / 2),
+        startAngle, sweepAngle, useCenter, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
 
